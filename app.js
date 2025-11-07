fae66b3229091cf6bb17a94aa3da4543d83685df
muhammad jawad <mjawaddayoo@gmail>
2025-11-07 19:53:43 +0500

    add: more loop

--- app.js
+++ app.js
@@ -7,21 +7,44 @@
 //     console.log ('minor');
 
 // }
-let marks = prompt("Please enter your marks");
-if (marks < 0 || marks > 100){
-    alert("Invalid Marks")
-}
+// let marks = prompt("Please enter your marks");
+// if (marks < 0 || marks > 100){
+//     alert("Invalid Marks")
+// }
 
-else if(marks >= 90){
-    console.log("Congratulations! You got A+ grade")
-}
-else if(marks >= 80){
-    console.log("Hurray! You got A garde")
-}
-else if(marks >= 70){
-    console.log('You got B garde')
-}
-else if(marks >= 50){
-    console.log('You got C garde')
-}
-else console.log('fail');
+// else if(marks >= 90){
+//     console.log("Congratulations! You got A+ grade")
+// }
+// else if(marks >= 80){
+//     console.log("Hurray! You got A garde")
+// }
+// else if(marks >= 70){
+//     console.log('You got B garde')
+// }
+// else if(marks >= 50){
+//     console.log('You got C garde')
+// }
+// else console.log('fail');
+
+                             // for loop
+// for(let i=1; i<=2; i++){
+//     console.log("count:",i);
+// }
+
+                           // while loop
+//  let count=1;
+//  while(count<= 3){
+//     console.log (count);
+//     count++;
+//  }
+
+// for(let i=1;  i<=10; i++){
+//    if(i===5){
+//       continue;
+//    }
+//    alert(i);
+// }
+                             // function
+function greet(){console.log("hello")}
+greet();
+
